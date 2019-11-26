00ae4e01ce0a337ae1ec32bc3fccecc1719a6352
St-andrey <40577453+St-andrey@users.noreply.github.com>
2019-11-26 15:21:10 +0300

    an old landing would be refuctored and finished

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    $('.slider-items').slick({
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 1,
+        autoplay: true,
+        dots: true,
+        arrows: false,
+    });
+});
