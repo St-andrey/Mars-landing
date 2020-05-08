05617e06eba4395756b0e93020962d5b2eb3e220
St-andrey <40577453+St-andrey@users.noreply.github.com>
2020-05-08 22:30:34 +0300

    responsive layout put in progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 
-$(function(){
+$(function () {
     $('.slider').slick({
         vertical: true,
         verticalSwiping: true,
@@ -7,5 +7,35 @@ $(function(){
         dots: true,
         arrows: false,
         dotsClass: "my-dots",
+    });
+    
+    $('.header-slider').slick({
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 1,
+        dots: true,
+        arrows: false,
+        
+        // dotsClass: "my-dots",
+    });
+
+    $('.header-top__burger-open').on('click', function () {
+        // $('.intro-menu').addClass('intro-menu--active');
+        $('.hidden-menu').animate({'left':'0'}, 1000);
+        
+    });
+    $('.hidden-menu__burger-close').on('click', function(){
+        // $('.intro-menu').removeClass('intro-menu--active');
+        $('.hidden-menu').animate({'left':'-200%'}, 1000);
+    });
+
+    $('.intro-top__burger-open').on('click', function () {
+        // $('.intro-menu').addClass('intro-menu--active');
+        $('.intro-menu').animate({'left':'0'}, 1000);
+        
+    });
+    $('.intro-menu__burger-close').on('click', function(){
+        // $('.intro-menu').removeClass('intro-menu--active');
+        $('.intro-menu').animate({'left':'-100%'}, 1000);
     });
 });
